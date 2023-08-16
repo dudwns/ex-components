9642cad2e2dd19987171114c6790bf9ef6746569
yeongjun Kim <kyjun519@gmail.com>
2023-08-17 03:04:38 +0900

    feat: Image 컴포넌트 구현

---
+++ src/stories/Image.stories.js
@@ -0,0 +1,73 @@
+import Image from "../components/Image";
+
+export default {
+  title: "Component/Image",
+  component: Image,
+  args: {
+    lazy: false,
+    block: false,
+    src: "https://picsum.photos/200",
+    placeholder: "https://via.placeholder.com/200",
+    threshold: 0.5,
+    width: 200,
+    height: 200,
+    mode: "cover",
+  },
+  argTypes: {
+    lazy: {
+      // defaultValue: false,
+      control: { type: "boolean" },
+    },
+    block: {
+      // defaultValue: false,
+      control: { type: "boolean" },
+    },
+    src: {
+      name: "src",
+      type: { name: "string", require: true },
+      // defaultValue: "https://picsum.photos/200",
+      control: { type: "text" },
+    },
+    placeholder: {
+      name: "src",
+      type: { name: "string" },
+      // defaultValue: "https://via.placeholder.com/200",
+      control: { type: "text" },
+    },
+    threshold: {
+      type: { name: "number" },
+      // defaultValue: 0.5,
+      control: { type: "number" },
+    },
+    width: {
+      // defaultValue: 200,
+      control: { type: "range", min: 200, max: 600 },
+    },
+    height: {
+      // defaultValue: 200,
+      control: { type: "range", min: 200, max: 600 },
+    },
+    alt: {
+      control: "string",
+    },
+    mode: {
+      // defaultValue: "cover",
+      options: ["cover", "fill", "contain"],
+      control: { type: "inline-radio" },
+    },
+  },
+};
+
+export const Default = (args) => {
+  return <Image {...args} />;
+};
+
+export const Lazy = (args) => {
+  return (
+    <div>
+      {Array.from(new Array(20), (_, k) => k).map((i) => (
+        <Image {...args} lazy block src={`${args.src}?${i}`} key={i} />
+      ))}
+    </div>
+  );
+};
